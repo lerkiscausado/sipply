041c8bb2c16ce469d341651cf4ac010f5fefa250
causadol <lerkiscausado@gmail.com>
2022-12-19 15:25:41 -0500

    primer commit sipply

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import express from 'express';
+
+
+const app =express();
+
+//app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+export default app;
---
+++ src/index.js
@@ -0,0 +1,20 @@
+import app from './app.js'
+//import {sequelize} from './database/conexion.js'
+
+//import './models/Departamentos.js'
+//import './models/Municipios.js'
+//import './models/Marcas.js'
+//import './models/Presentacion.js'
+//import './models/Empresas.js'
+//import './models/TipoEmpresa.js'
+
+async function main(){
+    try {
+  //      await sequelize.sync({alter:true});
+        console.log('Connection has been established successfully.');
+        app.listen(3000)
+      } catch (error) {
+        console.error('Unable to connect to the database:', error);
+      }    
+}
+main();
